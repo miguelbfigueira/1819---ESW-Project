e8b19f7ca44a0721390dbf4824cd9f314fb3c7df
miguelbfigueira <miguelbfigueira@users.noreply.github.com>
2018-11-13 23:37:27 +0000

    Add project files.

---
+++ 1819 - ESW Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _1819___ESW_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
